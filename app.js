134d8fdc903266735a9ee6e3fea525a883de71e6
MrTwisterGm <matyasezeber@gmail.com>
2021-11-09 13:23:10 -0300

    Se sube el proyecto terminado

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+app.listen(3000, (req, res) => {console.log("App en el puerto 3000")});
+
+const indexRoute = require('./routes/indexRoute');
+const detalleRoute = require('./routes/detalleRoute');
+const loginRoute = require('./routes/loginRoute');
+const registroRoute = require('./routes/registroRoute');
+
+app.use(express.static('./public'));
+
+app.use(session({secret: 'Buuuuuenas',
+resave: true,
+saveUninitialized: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine', 'ejs');
+app.set('views')
+
+app.use('/', indexRoute);
+app.use('/detalle', detalleRoute);
+app.use('/registrar', registroRoute);
+app.use('/login',loginRoute);
+app.get('/logout', (req, res)=> {
+    req.session.destroy()
+    res.redirect('/')
+
+})
---
+++ models/Genero.js
@@ -0,0 +1,53 @@
+
+module.exports = (sequelize,dataTypes)=>{
+    let alias = 'Generos';
+    let cols = {
+    
+    id:{
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement:true,
+        allowNull: false
+    },
+    name:{
+        type: dataTypes.STRING,
+        allowNull: false
+    },
+    ranking: {
+        type: dataTypes.INTEGER,
+        allowNull: false
+    },
+    active: {
+        type: dataTypes.INTEGER,
+        allowNull: false
+    },
+    updated_at: {
+        type: dataTypes.DATE
+    },
+    created_at: {
+        type: dataTypes.DATE
+    }
+
+    };
+
+    let config = {
+        tableName: "genres",
+        timestamps: true,
+        paranoid: true,
+
+        updatedAt: 'updated_at',
+        createdAt: 'created_at'
+    }
+
+
+const Generos = sequelize.define(alias, cols, config);
+Generos.associate = function(models){
+
+    Generos.hasMany(models.Peliculas, {
+        as: 'pelicula',
+        foreignKey:'genre_id'
+    })
+}
+
+return Generos;
+}
